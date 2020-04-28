8922a3c2889a099e3180f08484fde45eaae80719
Guilherme Costa <tec.guilherme.dsc@gmail.com>
2020-04-28 17:51:04 -0300

    Updates

---
+++ precache-manifest.1d85b1edca40d3a42aaaeb8b967d7144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe61901a2f39fca7845ab2c31b5fbe5a",
+    "url": "/web-semanaomnistack10/index.html"
+  },
+  {
+    "revision": "3fafa37c97919a463480",
+    "url": "/web-semanaomnistack10/static/css/main.f794163c.chunk.css"
+  },
+  {
+    "revision": "d4886bf66854dc184280",
+    "url": "/web-semanaomnistack10/static/js/2.d647aa2a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/web-semanaomnistack10/static/js/2.d647aa2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fafa37c97919a463480",
+    "url": "/web-semanaomnistack10/static/js/main.a0775ccf.chunk.js"
+  },
+  {
+    "revision": "987ffc9641768a79a4e9",
+    "url": "/web-semanaomnistack10/static/js/runtime-main.53fea537.js"
+  }
+]);
